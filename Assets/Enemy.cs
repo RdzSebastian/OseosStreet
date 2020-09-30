1bb752125f509cab98fa2183b094df784049a998
rdzsebastian <rdzsebastian@gmail.com>
2020-09-30 20:43:34 -0300

    Adds life, punch and damage logic

---
+++ Assets/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator animator;
+    public int maxHealt = 100;
+    int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealt;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        // Play Animation
+        animator.SetTrigger("Hurt");
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy Died");
+
+        // Die animation
+        animator.SetBool("IsDead", true);
+
+        // Disable the enemy
+        GetComponent<Collider>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        this.enabled = false;
+
+
+    }
+}
---
+++ Assets/PlayerCombat.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+    public LayerMask enemyLayers;
+    public Transform attackPoint;
+
+    public float attackRange = 0.8f;
+    public int attackDamange = 40;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        // Play attack animation
+        animator.SetTrigger("Attack");
+
+        // Detect enemies in range of attack
+        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
+
+
+        // Damage them
+        foreach (Collider enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamange);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
